4a31e2283cd5bfffa565e5d5b524b5b692d5ce6e
Keshav Agarwal <archanab486@gmail.com>
2023-09-01 18:15:34 +0530

    Final

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var router = express.Router();
 var app = express();
 var mysql = require("mysql");
 var bodyParser = require("body-parser")
+var http = require("http")
+
+const server = http.createServer('./app')
 
 app.set("view engine", "ejs")
 
