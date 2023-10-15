8aaba753463383f39505393b7297f8fc5858cb2d
Keshav Agarwal <archanab486@gmail.com>
2023-10-15 14:57:30 +0530

    Final

--- solutions.cpp
+++ solutions.cpp
@@ -21,3 +21,57 @@ int main(){
         cout<<e/c+1<<endl;
     }
 }
+vector<string>arr;
+    int x=0;
+    for(int i=0;i<s.size();i++){
+        if(s[i]==' ' || i==s.size()-1){
+            string s1 = s.substr(x, i);
+            arr.push_back(s1);
+            x=i+1;
+        }
+    }
+    vector<int>arr1;
+    int y=1;
+    x=0;
+    for(int i=0;i<arr.size();i++){
+        x=x*10;
+            // if(arr[i]=="double"){
+            //     y=2;
+            // }
+            // else if(arr[i]=="triple"){
+            //     y=3;
+            // }
+            // else{
+            if(arr[i]=="one"){
+                x=x+1*y;
+            }
+            else if(arr[i]=="two"){
+                x=x+2*y;
+            }
+            else if(arr[i]=="three"){
+                x=x+3*y;
+            }
+            else if(arr[i]=="four"){
+                x=x+4*y;
+            }
+            else if(arr[i]=="five"){
+                x=x+5*y;
+            }
+            else if(arr[i]=="six"){
+                x=x+6*y;
+            }
+            else if(arr[i]=="seven"){
+                x=x+7*y;
+            }
+            else if(arr[i]=="eight"){
+                x=x+8*y;
+            }
+            else if(arr[i]=="nine"){
+                x=x+9*y;
+            }
+            // y=1;
+            // }
+    }
+    
+    string s1 = to_string(x);
+    return s1;
